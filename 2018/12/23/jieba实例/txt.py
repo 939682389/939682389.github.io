f6b037e823336636ca64d1d51178e8c5714d7d65
zjk <939682389@qq.com>
2018-12-23 16:57:23 +0800

    Site updated: 2018-12-23 16:57:20

---
+++ 2018/12/23/jieba实例/txt.py
@@ -0,0 +1,20 @@
+import jieba
+
+txt = open('v8.txt','r').read()
+words = jieba.lcut(txt)
+counts = {}
+for word in words:
+    if len(word) == 1:
+        continue
+    else:
+        counts[word] = counts.get(word,0) + 1
+        
+items = list(counts.items())
+
+items.sort(key=lambda x : x[1],reverse=True)
+#items.sort(reverse = True)
+for i in range(20):
+    word ,count =items[i]
+    print(word,count)
+#    print('{0:<10}{1:>5}'.format(word,count))
+
